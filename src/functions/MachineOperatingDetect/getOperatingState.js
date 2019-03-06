8b250d3b6f9f1ce06e2f0b1296f2c1e0bd9dc919
Tri Pham Minh <tri.pham@pycogroup.com>
2019-03-06 17:07:33 +0700

    feat: recognize miner which is operating for 30s

--- src/functions/MachineOperatingDetect/getOperatingState.js
+++ src/functions/MachineOperatingDetect/getOperatingState.js
@@ -4,41 +4,102 @@ const {
   isEmpty,
   find,
   map,
+  pathOr,
   reject,
   split,
 } = require('ramda');
+const SortedMap = require('collections/sorted-map');
 
 const OPERATION_REGEXS = [
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_INNER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_OUTER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_RIB_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_INNER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_OUTER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_RIB_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_CUTTER_ON$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_LEFT_LOADER_CURRENT$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_RIGHT_LOADER_CURRENT$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_TRAM_MODE$/g
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_INNER_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_OUTER_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_RIB_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_INNER_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_OUTER_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_RIB_ROTATION$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_CUTTER_ON$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_LEFT_LOADER_CURRENT$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_RIGHT_LOADER_CURRENT$/,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_TRAM_MODE$/
 ];
 
-module.exports = (tag, value) => {
+const LEAST_OPERATE_DURATION = 30000; // 30s
+
+// a map table of miner's operation timestamp
+// if map table is fulfilled it will remove oldest one
+class WorktimeMap {
+  constructor(MAX_SIZE = 100) {
+    this.__wmap = new SortedMap();
+    this.__MAX_SIZE = MAX_SIZE;
+  }
+
+  __pop() {
+    const store = pathOr({}, ['store'], this.__wmap);
+    invoker(0, 'shift')(store);
+  }
+  
+  __prepareMap() {
+    let length = this.__wmap.length;
+
+    while (length >= this.__MAX_SIZE) {
+      this.__pop();
+      length--;
+    }
+  }
+
+  set(key, value) {
+    this.__prepareMap();
+    return this.__wmap.set(key, value);
+  }
+
+  get(key) {
+    return this.__wmap.get(key);
+  }
+
+  delete(key) {
+    return this.__wmap.delete(key);
+  }
+}
+
+const wmap = new WorktimeMap();
+
+module.exports = (tag, value, timestamp) => {
+  let miner_state = 'waiting';
+
   // match tag with all pre-defined regexs
   const matched_regex = find(
     regex => invoker(1, 'test')(tag, regex),
     OPERATION_REGEXS
   );
 
-  if (!matched_regex || value !== 1) return null;
-
+  // get miner_number & cluster number from tag
   const TAG_SPLIT_REGEX = /[^0-9]+/g;
   const [cluster_number, miner_number] = compose(
     map(Number.parseInt),  // parse string to number
     reject(isEmpty),       // remove falsy
     split(TAG_SPLIT_REGEX) // get tokens
   )(tag);
+  
+  // get last operation from map table
+  const signature = `${cluster_number}.${miner_number}`;
+  const pre_timestamp = wmap.get(signature);
+  const operate_duration = timestamp - pre_timestamp;
+  
+  // update value in map table
+  const is_valid_operation = matched_regex && value === 1;
+  if (is_valid_operation) {
+    wmap.set(signature, timestamp);
+  } else {
+    wmap.delete(signature);
+  }
+
+  if (is_valid_operation && operate_duration >= LEAST_OPERATE_DURATION) {
+    miner_state = 'operating';
+  }
 
   return {
     miner_number,
-    cluster_number
+    cluster_number,
+    miner_state
   }
 };
--- src/functions/MachineOperatingDetect/index.js
+++ src/functions/MachineOperatingDetect/index.js
@@ -1,5 +1,6 @@
 const {
-  path
+  path,
+  pathOr
 } = require('ramda');
 const getOperatingState = require('./getOperatingState');
 
@@ -7,12 +8,13 @@ module.exports = async (context, queueItem) => {
   const parsed_item = JSON.parse(queueItem);
   const tag = path(['tag'], parsed_item);
   const value = path(['value'], parsed_item);
+  const timestamp = pathOr(0, ['timestamp'], parsed_item);
   
-  const operating_state = getOperatingState(tag, value);
+  const operating_state = getOperatingState(tag, value, timestamp);
 
   if (!operating_state) context.log(`'${tag}' didn't match any operation state`);
   else {
-    const { miner_number, cluster_number } = operating_state;
-    context.log(`#${miner_number} miner is operating in cluster ${cluster_number}`);
+    const { miner_number, cluster_number, miner_state } = operating_state;
+    context.log(`#${miner_number} miner is ${miner_state} in cluster ${cluster_number}`);
   }
 };
