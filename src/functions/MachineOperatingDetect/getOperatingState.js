f5469c7d34c2a59ccf3b6ef0e4329cece017fcf1
Tri Pham Minh <tri.pham@pycogroup.com>
2019-03-06 14:13:44 +0700

    refactor: move operating detection part into new module

---
+++ src/functions/MachineOperatingDetect/getOperatingState.js
@@ -0,0 +1,44 @@
+const {
+  compose,
+  invoker,
+  isEmpty,
+  find,
+  map,
+  reject,
+  split,
+} = require('ramda');
+
+const OPERATION_REGEXS = [
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_INNER_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_OUTER_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_RIB_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_INNER_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_OUTER_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_RIB_ROTATION$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_CUTTER_ON$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_LEFT_LOADER_CURRENT$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_RIGHT_LOADER_CURRENT$/g,
+  /^Cluster[0-9]*.Miner_ABM_[0-9]*_TRAM_MODE$/g
+];
+
+module.exports = (tag) => {
+  // match tag with all pre-defined regexs
+  const matched_regex = find(
+    regex => invoker(1, 'test')(tag, regex),
+    OPERATION_REGEXS
+  );
+
+  if (!matched_regex) return null;
+
+  const TAG_SPLIT_REGEX = /[^0-9]+/g;
+  const [cluster_number, miner_number] = compose(
+    map(Number.parseInt),  // parse string to number
+    reject(isEmpty),       // remove falsy
+    split(TAG_SPLIT_REGEX) // get tokens
+  )(tag);
+
+  return {
+    miner_number,
+    cluster_number
+  }
+};
--- src/functions/MachineOperatingDetect/index.js
+++ src/functions/MachineOperatingDetect/index.js
@@ -1,44 +1,16 @@
 const {
-  compose,
-  invoker,
-  isEmpty,
-  find,
-  map,
-  path,
-  reject,
-  split,
+  path
 } = require('ramda');
-
-const OPERATION_REGEXS = [
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_INNER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_OUTER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_LH_RIB_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_INNER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_OUTER_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_BOLTER_RH_RIB_ROTATION$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_CUTTER_ON$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_LEFT_LOADER_CURRENT$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_RIGHT_LOADER_CURRENT$/g,
-  /^Cluster[0-9]*.Miner_ABM_[0-9]*_TRAM_MODE$/g
-];
+const getOperatingState = require('./getOperatingState');
 
 module.exports = async (context, queueItem) => {
   const tag = path(['tag'], JSON.parse(queueItem));
+  
+  const operating_state = getOperatingState(tag);
 
-  // match tag with all pre-defined regexs
-  const matched_regex = find(
-    regex => invoker(1, 'test')(tag, regex),
-    OPERATION_REGEXS
-  );
-
-  if (!matched_regex) return context.log(`'${tag}' didn't match any operation state`);
-
-  const TAG_SPLIT_REGEX = /[^0-9]+/g;
-  const [cluster_number, miner_number] = compose(
-    map(Number.parseInt),  // parse string to number
-    reject(isEmpty),       // remove falsy
-    split(TAG_SPLIT_REGEX) // get tokens
-  )(tag);
-
-  context.log(`#${miner_number} miner is operating in cluster ${cluster_number}`);
+  if (!operating_state) context.log(`'${tag}' didn't match any operation state`);
+  else {
+    const { miner_number, cluster_number } = operating_state;
+    context.log(`#${miner_number} miner is operating in cluster ${cluster_number}`);
+  }
 };
